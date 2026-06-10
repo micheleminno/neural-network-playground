4e4a3e3a092de3b64967a9cfd48ee08d4f2ad997
michele minno <michele.minno@protonmail.com>
2026-06-10 19:03:43 +0200

    Update Supabase key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://ajdnlxgbkkwihqiflyby.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_QUI_LA_TUA_CHIAVE";
+const SUPABASE_KEY = "sb_publishable_CmojWuYCYnwEh0kgfZ5fmw_DfsQ7HrE";
 
 async function testSupabase() {
   const res = await fetch(`${SUPABASE_URL}/rest/v1/networks?select=*`, {
